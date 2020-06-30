5cf09c23c144433c8c95bf983932e27da62dac9a
Facundo Araujo <facuaaraujo@gmail.com>
2020-06-30 14:54:05 -0300

    Updates

---
+++ precache-manifest.df43a5207ac38e78de38aec72101376f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bbfc3e4f286c139a0cc948f4938ad4",
+    "url": "/react-wallet-localStorage/index.html"
+  },
+  {
+    "revision": "9081712197837561a3a0",
+    "url": "/react-wallet-localStorage/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "220efd40398b240662d6",
+    "url": "/react-wallet-localStorage/static/css/main.1e1cb348.chunk.css"
+  },
+  {
+    "revision": "9081712197837561a3a0",
+    "url": "/react-wallet-localStorage/static/js/2.246be451.chunk.js"
+  },
+  {
+    "revision": "596e4823497e45004d2db4c50beba3f3",
+    "url": "/react-wallet-localStorage/static/js/2.246be451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "220efd40398b240662d6",
+    "url": "/react-wallet-localStorage/static/js/main.02aa938e.chunk.js"
+  },
+  {
+    "revision": "547b336949b6e52f030b",
+    "url": "/react-wallet-localStorage/static/js/runtime-main.d06fb16a.js"
+  },
+  {
+    "revision": "036ce7cad71f85ef39ae3b83f71c6f33",
+    "url": "/react-wallet-localStorage/static/media/visa-logo.036ce7ca.svg"
+  }
+]);
